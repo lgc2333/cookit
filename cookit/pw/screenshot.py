83581d112e39baf4b11738a8b526990c27541c4f
student_2333 <lgc2333@126.com>
2024-07-31 22:57:48 +0800

    up

--- cookit/pw/screenshot.py
+++ cookit/pw/screenshot.py
@@ -1,9 +1,30 @@
-from typing import Optional
+from typing import Any, Awaitable, Callable, Literal, Optional, Union
+from typing_extensions import TypeAlias
 
 from playwright.async_api import Page
 
+WaitFunction: TypeAlias = Callable[[Page], Awaitable[Any]]
+WaitForType: TypeAlias = Union[
+    Literal["domcontentloaded", "load", "networkidle"],
+    WaitFunction,
+    None,
+]
 
-async def screenshot_selector(page: Page, selector: str, **kwargs) -> bytes:
+
+async def _wait_for(wait_type: WaitForType, page: Page):
+    if callable(wait_type):
+        await wait_type(page)
+    elif wait_type:
+        await page.wait_for_load_state(wait_type)
+
+
+async def screenshot_selector(
+    page: Page,
+    selector: str,
+    wait_type: WaitForType = "load",
+    **kwargs,
+) -> bytes:
+    await _wait_for(wait_type, page)
     elem = await page.query_selector(selector)
     if not elem:
         raise ValueError("Element not found")
@@ -14,9 +35,11 @@ async def screenshot_html(
     page: Page,
     html: str,
     selector: Optional[str] = None,
+    wait_type: WaitForType = "load",
     **kwargs,
 ) -> bytes:
     await page.set_content(html)
     if selector:
-        return await screenshot_selector(page, selector, **kwargs)
+        return await screenshot_selector(page, selector, wait_type, **kwargs)
+    await _wait_for(wait_type, page)
     return await page.screenshot(**kwargs)
